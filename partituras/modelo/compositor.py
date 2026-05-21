b63f594bc40410c506f77eea660b6e2b1f78ae07
Udem <udem@gmail.com>
2026-05-21 16:57:29 -0500

    importacion de metodos abstractos

---
+++ partituras/modelo/compositor.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+from partituras.modelo.errores import (
+    ContieneNumero,
+    ContieneCaracterInvalido,
+    SinNotas,
+    EspacioMultiple,
+    EspacioBordes,
+)
