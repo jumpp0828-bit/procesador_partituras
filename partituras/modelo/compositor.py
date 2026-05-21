df2aaefb367fb2b0da593173fed8bb3f5421ca57
Udem <udem@gmail.com>
2026-05-21 17:01:36 -0500

    inicio clase ReglaTransformacion, encontrar numeros

--- partituras/modelo/compositor.py
+++ partituras/modelo/compositor.py
@@ -7,3 +7,27 @@ from partituras.modelo.errores import (
     EspacioMultiple,
     EspacioBordes,
 )
+
+class ReglaTransformacion(ABC):
+
+    def __init__(self, token):
+        self.token = token
+
+    @abstractmethod
+    def transformar(self, partitura):
+        pass
+
+    @abstractmethod
+    def revertir(self, partitura):
+        pass
+
+    @abstractmethod
+    def partitura_valida(self, partitura):
+        pass
+
+    def encontrar_numeros_partitura(self, partitura):
+        return [
+            (i, caracter)
+            for i, caracter in enumerate(partitura)
+            if caracter.isdigit()
+        ]
